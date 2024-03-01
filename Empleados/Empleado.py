20dc1a632123fd960d7de0d9f23999345125dcc0
lxiscxstillo <luisestebancastillopedroza90@gmail.com>
2024-02-29 19:51:23 -0500

    29/02/2024

--- Empleados/Empleado.py
+++ Empleados/Empleado.py
@@ -1,3 +1,5 @@
+from Fecha import Fecha
+
 class Empleado:
     #aqui va el codigo
 
@@ -12,6 +14,12 @@ class Empleado:
     ----------------------------------------------------------'''
     sexo=0
     salario=0
+
+    '''----------------------------------------------------------
+    # Asociaciones
+    ----------------------------------------------------------'''
+    fechaNacimiento = Fecha()
+    fechaIngreso = Fecha()
 
     '''----------------------------------------------------------
     # Metodos
@@ -32,3 +40,24 @@ class Empleado:
         nSalario = self.salario+aumento
         self.salario = nSalario
         return "Su nuevo salario es " + self.salario
+    
+    def DuplicarSalario(self):
+        #forma 1
+        nuevoSalario = self.salario*2
+        self.salario = nuevoSalario
+
+        # #forma 2
+        # self.salario *= 2
+
+    def CalcularSalarioAnual(self):
+        #forma 1
+        salarioAnual = self.salario*12
+        return salarioAnual
+    
+        # #forma 2
+        # return self.salario*12
+    
+    def ConsultarCumpleanios(self):
+        return self.fechaNacimiento.ConsultarDia()
+    
+    
--- Empleados/Fecha.py
+++ Empleados/Fecha.py
@@ -8,3 +8,16 @@ class Fecha:
     dia=0
     mes=0
     anio=0
+
+    '''-------------------------------------------------------------------------------------------
+    # Metodos
+    --------------------------------------------------------------------------------------------'''
+
+    def ConsultarDia(self):
+        return self.dia
+    
+    def ConsultarMes(self):
+        return self.mes
+    
+    def ConsultarAnio(self):
+        return self.anio
